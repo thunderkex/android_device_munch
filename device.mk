6cdd5df1ccecc7a52d299a3dbb911a2ad23fd83d
Thunderkex <thunderkex@gmail.com>
2024-10-15 12:53:20 +0700

    munch: Remove unwated package

--- device.mk
+++ device.mk
@@ -456,5 +456,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+# Remove unwanted packages
+PRODUCT_PACKAGES += \
+    RemovePackages
+    
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/munch/munch-vendor.mk)
